420c1ba1d38fda3d068c9089881abc26819c2ef5
ihyamars07 <ihyamars@students.amikom.ac.id>
2022-10-13 20:57:30 +0700

    Make template with express js

--- app.js
+++ app.js
@@ -1,22 +1,83 @@
-const fs = require("fs");
+
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+const port = process.env.PORT || 8000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
+app.get('/', (req, res) => {
+  res.sendFile('public/view/index.html', {
+    root: __dirname
+  });
+});
 
 // Use the saved values
 const client = new Client({
-  puppeteer: { headless: true },
-  authStrategy: new LocalAuth({ dataPath: "./session" }),
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
+  authStrategy: new LocalAuth()
 });
 
-client.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
-});
+client.initialize();
 
-// Save session values to the file upon successful auth
-client.on("authenticated", (session) => {});
+// Socket IO
+io.on('connection', function(socket) {
+  socket.emit('message', 'Connecting...');
 
-client.on("ready", () => {
-  console.log("Client is ready!");
+  client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'QR Code received, scan please!');
+    });
+  });
+
+  client.on('ready', () => {
+    socket.emit('ready', 'Whatsapp is ready!');
+    socket.emit('message', 'Whatsapp is ready!');
+  });
+
+  client.on('authenticated', () => {
+    socket.emit('authenticated', 'Whatsapp is authenticated!');
+    socket.emit('message', 'Whatsapp is authenticated!');
+    console.log('AUTHENTICATED');
+  });
+
+  client.on('auth_failure', function(session) {
+    socket.emit('message', 'Auth failure, restarting...');
+  });
+
+  client.on('disconnected', (reason) => {
+    socket.emit('message', 'Whatsapp is disconnected!');
+    client.destroy();
+    client.initialize();
+  });
 });
 
 client.on("message", (message) => {
@@ -40,4 +101,8 @@ client.on("message", async (msg) => {
   }
 });
 
-client.initialize();
+
+
+server.listen(port, function() {
+  console.log('App running on http://localhost:' + port);
+});
